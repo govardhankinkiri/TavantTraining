7425c5746cc225953f603b9df7a43e875b12bc5c
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-08-16 11:53:43 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/Factorial.java
+++ Tavant Pre-Joining Training/src/Day1/Factorial.java
@@ -3,24 +3,30 @@ package day1;
 import java.util.Scanner;
 
 public class Factorial {
+	public int CalculateFactorial(int number)
+{
+	int i,p=1;
+	if (number<0) return 0;
 	
-static int CalculateFactorial(int a)
+	for(i=1;i<=number;i++)
 	{
-		int i,p=1;
-		for(i=1;i<=a;i++)
-		{
-		p=p*i;
-		}
- return p;
+	p=p*i;
 	}
+return p;
+}
+	
+
         public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int number;
 		System.out.println("Enter The No:");
 		number=scanner.nextInt();
-		System.out.println("Factorial = "+CalculateFactorial(number));
+		Factorial factorial = new Factorial();
+		System.out.println("Factorial = "+factorial.CalculateFactorial(number));
 		scanner.close();
 		}
+        
+        
 
 }
 
