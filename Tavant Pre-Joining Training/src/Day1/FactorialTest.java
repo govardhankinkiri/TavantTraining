bed36b8cc71aad47145febef7f70b4fdd1b2cf80
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-08-16 12:00:36 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/FactorialTest.java
+++ Tavant Pre-Joining Training/src/Day1/FactorialTest.java
@@ -1,16 +1,19 @@
-package Day1;
+package day1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class FactTest {
+lass FactTest {
 
 	@Test
 	void test() {
 		Factorial fact =new Factorial();
 		int out = fact.CalculateFactorial(5);
 		assertEquals(120,out);
+		int out1 = fact.CalculateFactorial(4);
+		assertEquals(22,out);
+		
 	}
 
 }
